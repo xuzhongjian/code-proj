8b8b61d6d3de725127c0c9993bdb669995afd213
xuzhongjian <xuzhongjian@xiaomi.com>
2021-01-02 11:31:40 +0800

    update
    update

--- do-thread/src/main/java/com/xuzhongjian/thread/Main.java
+++ do-thread/src/main/java/com/xuzhongjian/thread/Main.java
@@ -4,4 +4,8 @@ package com.xuzhongjian.thread;
  * @author zjxu97 at 1/2/21 11:00 AM
  */
 public class Main {
+    public static void main(String[] args) {
+        Thread thread = new Thread(null, () -> {
+        });
+    }
 }
