f5577114647502cf32c15b5bad0c8a8096f7eb80
xuzhongjian <xuzhongjian@xiaomi.com>
2021-01-05 00:23:26 +0800

    update

---
+++ do-thread/src/main/java/com/xuzhongjian/thread/model/SynchronizedTest.java
@@ -0,0 +1,12 @@
+package com.xuzhongjian.thread.model;
+
+/**
+ * @author zjxu97 at 1/3/21 11:17 PM
+ */
+public class SynchronizedTest {
+
+    public static synchronized void test() {
+        // TODO
+    }
+
+}
