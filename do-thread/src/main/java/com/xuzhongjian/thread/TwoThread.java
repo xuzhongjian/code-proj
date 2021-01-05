9b629f5c0b11f1d4a8e07991d264d35610ecb87c
xuzhongjian <xuzhongjian@xiaomi.com>
2021-01-05 20:47:56 +0800

    two thread

---
+++ do-thread/src/main/java/com/xuzhongjian/thread/TwoThread.java
@@ -0,0 +1,32 @@
+package com.xuzhongjian.thread;
+
+/**
+ * @author zjxu97 at 1/5/21 8:17 PM
+ */
+public class TwoThread {
+    private static final Object o = new Object();
+    private static volatile int i = 0;
+
+    public static void main(String[] args) {
+        new Thread(new Runner(), "a").start();
+        new Thread(new Runner(), "b").start();
+    }
+
+    private static class Runner implements Runnable {
+        @Override
+        public void run() {
+            String threadName = Thread.currentThread().getName();
+            while (true) {
+                synchronized (o) {
+                    try {
+                        o.notify();
+                        System.out.println(threadName);
+                        o.wait();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+}
