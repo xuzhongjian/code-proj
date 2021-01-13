7c526f66695894da7fbf1b5fbf990e8bcb4909f6
xuzhongjian <xuzhongjian@xiaomi.com>
2021-01-13 20:30:58 +0800

    Update Main.java

--- do-common/src/main/java/com/xuzhongjian/Main.java
+++ do-common/src/main/java/com/xuzhongjian/Main.java
@@ -1,14 +1,19 @@
 package com.xuzhongjian;
 
-import java.util.Collection;
+import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author zjxu97 at 12/25/20 11:20 AM
  */
 public class Main {
     public static void main(String[] args) {
-        ExecutorService executorService =  ExecutorService;
+        ExecutorService executorService = new ThreadPoolExecutor(10, 10,
+                100L, TimeUnit.MINUTES, new ArrayBlockingQueue<Runnable>(10, true));
+
+        executorService.submit(()->{});
 
     }
 }
