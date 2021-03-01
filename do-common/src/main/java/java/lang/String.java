cd59c1b1dbfc44e45a422c76475b430828430bf5
xuzhongjian <xuzhongjian@xiaomi.com>
2021-03-01 15:08:27 +0800

    init

---
+++ do-common/src/main/java/java/lang/String.java
@@ -0,0 +1,9 @@
+package java.lang;
+
+/**
+ * @author zjxu97 at 1/14/21 10:16 PM
+ */
+public class String {
+    public String() {
+    }
+}
---
+++ do-common/src/main/java/java/lang/xuzhongjian.java
@@ -0,0 +1,9 @@
+package java.lang;
+
+/**
+ * @author zjxu97 at 1/15/21 11:01 AM
+ */
+public class xuzhongjian {
+    public xuzhongjian() {
+    }
+}
